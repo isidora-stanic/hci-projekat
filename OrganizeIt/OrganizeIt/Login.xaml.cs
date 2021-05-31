96d3da51b97ae7bd865ed86a99600f427c0cfc05
Matija <matija.matovic99@gmail.com>
2021-05-31 15:46:39 +0200

    Dodat prikaz korisnika i organizatora

--- OrganizeIt/OrganizeIt/Login.xaml.cs
+++ OrganizeIt/OrganizeIt/Login.xaml.cs
@@ -27,7 +27,7 @@ namespace OrganizeIt
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            NavigationService.Navigate(new AccountsList());
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
